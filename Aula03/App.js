4aba8519d03bba9d792bc758da4ee44d7b051122
MatheusCEvangelista <163679237+MatheusCEvangelista@users.noreply.github.com>
2025-08-28 23:24:19 +0000

    Configuraçao do ambiente de desenvolvimento

---
+++ Aula03/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.texto} >Aula 03- Configuraçao inicial do VsCode</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto : {
+    fontSize: 20,
+    color : 'red',
+    fontWeight : 'bold'
+  }
+});
