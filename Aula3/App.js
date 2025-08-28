a6ca1bb21faebdbdfeec8141a29dc93be2f4570d
Italo Veríssimo Barbosa <italoweb99@gmail.com>
2025-08-28 23:15:25 +0000

    (28/08) Configuração do ambiente de desenvolvimento

---
+++ Aula3/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+   
+    justifyContent: 'center',
+  },
+  text:{
+    textAlign: "center",
+     fontSize: 20,
+    fontFamily: "Comic Sans MS",
+  }
+});
