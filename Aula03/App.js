abc89f379fbcd175709ce9b9a9e03d46397d7873
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-08-28 23:14:20 +0000

    (28/08/) Configuração do ambiente de desenvolvimento

---
+++ Aula03/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aplicação carregada - Aula 03</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8dfdfff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
