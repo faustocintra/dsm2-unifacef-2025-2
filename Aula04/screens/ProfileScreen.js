ad651000b07125a4a99e0fb94a656c441be86744
GabeCarbone2 <121464484+GabeCarbone2@users.noreply.github.com>
2025-09-04 23:08:14 +0000

    (04/09) Atividade Aula 04

---
+++ Aula04/screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function ProfileScreen({ navigation }){
+    return(
+        <View style={styles.container}>
+        <Text style={styles.titulo}>Tela do Perfil</Text>
+        <Button title="Voltar" onPress={() => navigation.goBack()} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    titulo: { fontSize: 26, color: '#ff007bff' }
+    });
