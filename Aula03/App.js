e1b05cb3bca84c997fa14a7d91236309c80034c9
Enzo Alvarenga Mariano <136367820+EnzoAlv@users.noreply.github.com>
2025-09-04 23:21:29 +0000

    Aula 04

--- Aula03/App.js
+++ Aula03/App.js
@@ -4,17 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Configuração do Ambiente de Desenvolvimento - Aula 03</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Alteração no projeto para atividade da Aula 04</Text>
+      <Text style={styles.subtitle}>Design e Textos alterados.</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e1e1e', 
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20, 
+  },
+  title: {
+    fontSize: 32, 
+    fontWeight: 'bold', 
+    color: '#fff', 
+    marginBottom: 8, 
+  },
+  subtitle: {
+    fontSize: 18, 
+    color: '#a0a0a0', 
   },
 });
