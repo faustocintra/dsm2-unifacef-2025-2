eb869a4102b2484d1a26eb6683c2c31da16c7f45
Eduardo Cintra <82892813+eduardocintr@users.noreply.github.com>
2025-09-04 22:23:25 +0000

    Exercício Aula

--- Aula03/App.js
+++ Aula03/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Configuração do ambiente de desenvolvimento - Aula 03</Text>
+      <Text>Exercício Aula</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
