2018612497320fa6d6d98f3b9773e39e973fd7f0
Gdavij <gdavijac@gmail.com>
2025-09-11 23:40:00 -0300

    aula-04/2

--- Aula04/App.js
+++ Aula04/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/home-screen';
 import DetailsScreen from './screens/details-screen';
+import ProfileScreen from './screens/profile-screen'
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Início" component={HomeScreen} />
         <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
