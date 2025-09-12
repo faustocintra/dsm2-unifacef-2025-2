8670208626c4f8b858493c78f7d948c8f3f19c77
SamuelLago <Samuelsdlago@gmail.com>
2025-09-12 20:02:45 -0300

    Aula 05

---
+++ Aula05/screens/ProfileScreen.js
@@ -0,0 +1,16 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Image, StyleSheet, Text, View } from 'react-native';
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/avatar.png')} style={styles.avatar} />
+      <Text style={styles.nome}>João Silva</Text>
+      <Ionicons name="settings" size={30} color="gray" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  avatar: { width: 100, height: 100, borderRadius: 50 },
+  nome: { fontSize: 22, fontWeight: 'bold', marginVertical: 10 },
+});
